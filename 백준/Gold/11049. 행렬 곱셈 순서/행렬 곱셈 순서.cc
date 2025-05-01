3f6c8f52820e112e763674ca3676a220ead8666e
Kang Minje <63361616+10000Je@users.noreply.github.com>
2025-05-02 00:25:27 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 24 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -6,29 +6,29 @@
 #define MIN(a, b) ((a < b) ? a : b)
 using namespace std;
 
-int dp[501][501];
+int dp[500][500];
 
 int main() {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
     int n;
     cin >> n;
-    vector<pair<int, int>> arr(n+1, make_pair(0, 0));
-    for(int i=1; i<=n; i++) {
+    vector<pair<int, int>> arr(n, make_pair(0, 0));
+    for(int i=0; i<n; i++) {
         int r, c;
         cin >> r >> c;
         arr[i] = make_pair(r, c);
     }
     // init
-    for(int i=0; i<=n; i++) {
+    for(int i=0; i<n; i++) {
         dp[i][i] = 0;
     }
-    for(int i=1; i<=n-1; i++) {
+    for(int i=0; i<n-1; i++) {
         dp[i][i+1] = arr[i].first * arr[i].second * arr[i+1].second;
     }
     // dp
     for(int i=2; i<n; i++) {
-        for(int j=1; j<=n-i; j++) {
+        for(int j=0; j<n-i; j++) {
             dp[j][j+i] = dp[j+1][j+i] + arr[j].first * arr[j].second * arr[j+i].second;
             for(int k=j+1; k<j+i; k++) {
                 int cost = arr[j].first * arr[k].second * arr[j+i].second;
@@ -36,6 +36,6 @@ int main() {
             }
         }
     }
-    cout << dp[1][n] << '\n';
+    cout << dp[0][n-1] << '\n';
     return 0;
 }
