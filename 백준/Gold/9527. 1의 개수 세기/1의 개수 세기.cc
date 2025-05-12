ecde4c73471e7668ad8f55c1ca73ef31e6eb727f
Kang Minje <63361616+10000Je@users.noreply.github.com>
2025-05-13 01:16:26 +0900

    [Gold II] Title: 1의 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
@@ -1,8 +1,8 @@
 // no.9527: 1의 개수 세기 (G2)
 
 #include <iostream>
-#include <string>
 #include <vector>
+#define MAX 60
 using namespace std;
 
 typedef long long int64;
@@ -13,9 +13,9 @@ int main()
 {
     int64 a, b;
     cin >> a >> b;
-    vector<int64> dp(60, 0);
+    vector<int64> dp(MAX, 0);
     dp[0]= 1;
-    for(int i=1; i<60; i++)
+    for(int i=1; i<MAX; i++)
         dp[i] = (1LL<<i) + dp[i-1]*2;
     cout << count(b, dp) - count(a-1, dp) << '\n';
     return 0;
@@ -24,7 +24,7 @@ int main()
 int64 count(int64 n, vector<int64>& dp) 
 {
     int64 result = 0;
-    for(int i=55; i>0; i--)
+    for(int i=MAX; i>0; i--)
     {   
         if((n&(1LL<<i)) == 0)
             continue;
