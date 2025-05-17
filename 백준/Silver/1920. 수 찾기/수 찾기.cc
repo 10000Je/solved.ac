b9c3a3367494097ae676c74005415a765e1ab17f
Kang Minje <63361616+10000Je@users.noreply.github.com>
2025-05-17 18:49:41 +0900

    [Silver IV] Title: 수 찾기, Time: 64 ms, Memory: 2452 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,40 +1,83 @@
-// no.1920: 수 찾기 (S4)
+/**
+ * Baekjoon 1920. 수 찾기
+ * Hashing
+ * 202134202 강민제
+ */
 
-#include <cstdio>
-#include <unordered_set>
-#include <cstring>
-#include <cstdlib>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 170003 // prime number
+#define TRUE 1
+#define FALSE 0
 
-char a[1'200'000];
-char b[1'200'000];
+typedef struct HashTable
+{
+    int* data;
+    int* valid;
+} HashTable;
 
-int main() {
+void insert(HashTable* hashTable, int val);
+
+int check(HashTable* hashTable, int val);
+
+int main()
+{
+    int* data = (int*)malloc(sizeof(int)*SIZE);
+    int* valid = (int*)malloc(sizeof(int)*SIZE);
+    for(int i=0; i<SIZE; i++)
+        valid[i] = FALSE;
+    HashTable hashTable = {data, valid};
+
     int n;
     scanf("%d", &n);
-    getchar();
-    fgets(a, 1'200'000, stdin);
-
-    unordered_set<int> arr;
-    char* temp = strtok(a, " ");
-    while(temp != NULL) {
-        arr.insert(atoi(temp));
-        temp = strtok(NULL, " ");
+    for(int i=0; i<n; i++)
+    {
+        int val;
+        scanf("%d", &val);
+        insert(&hashTable, val);
     }
 
     int m;
     scanf("%d", &m);
-    getchar();
-    fgets(b, 1'200'000, stdin);
-
-    temp = strtok(b, " ");
-    while(temp != NULL) {
-        if(arr.find(atoi(temp)) != arr.end()) {
-            printf("1\n");
-        } else {
-            printf("0\n");
-        }
-        temp = strtok(NULL, " ");
+    for(int i=0; i<m; i++)
+    {
+        int num;
+        scanf("%d", &num);
+        printf("%d\n", check(&hashTable, num));
     }
     return 0;
+}
+
+int hash1(int val)
+{
+    return abs(val) % SIZE;
+}
+
+int hash2(int val)
+{
+    return 1;
+}
+
+void insert(HashTable* hashTable, int val)
+{
+    if(check(hashTable, val))
+        return;
+    int i = hash1(val);
+    while(hashTable->valid[i] == TRUE)
+        i = (i + hash2(val)) % SIZE;
+    hashTable->data[i] = val;
+    hashTable->valid[i] = TRUE;
+}
+
+int check(HashTable* hashTable, int val)
+{
+    int i = hash1(val);
+    while(hashTable->valid[i] == TRUE)
+    {
+        if(hashTable->data[i] == val)
+            return TRUE;
+        else
+            i = (i + hash2(val)) % SIZE;
+    }
+    return FALSE;
 }
